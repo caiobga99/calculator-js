80fbbd0a83b4f2d96f82e82e2ad3c97eb92d2dcf
caiobga99 <105997288+caiobga99@users.noreply.github.com>
2022-12-25 08:32:03 -0300

    Arrumando bugs e deixando possivel a reutilização de funções.
    Arrumando bugs e deixando possivel a reutilização de funções.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll("button");
 let arr = [];
+//const arr = [];
 let resultado = "";
 let display = document.querySelector(".resultado");
 const currentOperand = "";
@@ -8,42 +9,74 @@ const previousOperand = "";
 
 buttons.forEach(e => {
     e.addEventListener("click", i => {
+        const twoOperators = () => {
+            //função que verifica se ha um operador matematico seguido de outro
+            const operatorsName = ["+", "-", "*", "÷"];
+            for (let i = 0; i < operatorsName.length; i++) {
+                if (arr[arr.length - 1] == operatorsName[i] && arr[arr.length - 2] == operatorsName[i]) {
+                    console.log("2 operadores matematicos juntos");
+                    arr.pop();
+                }
+            }
+        }
 
+        const operatorMathematical = () => {
+            if (arr.length == 0) {
+                if (i.target.className == "sinal") {
+                    arr = [];
+                    arr.push(0);
+                    console.log(arr);
+                }
+            }
+
+        }
+        const updateDisplay = () => {
+            twoOperators();
+            operatorMathematical();
+            resultado = i.target.innerHTML;
+            arr.push(resultado);
+            display.innerHTML = arr.join('');
+        }
         switch (i.target.className) {
             case "num":
-                resultado = i.target.innerHTML;
-                arr.push(resultado);
-                display.innerHTML = arr.join('');
+                updateDisplay();
                 break;
             case "sinal":
                 const verificar = simbol => {
-                    if (arr.includes(simbol)) {
-                        console.log("sinal existente");
-                    } else {
+                    if (!arr.includes(simbol)) {
                         console.log("nao existe sinal");
                         if (arr.length == 0) {
                             arr.push(0);
                         }
-                        resultado = i.target.innerHTML;
-                        arr.push(resultado);
-                        display.innerHTML = arr.join('');
+                        updateDisplay();
                     }
+                }
+                const sinalInvalido = (sinal) => {
+                    if (arr[arr.length - 1] == sinal) {
+                        alert("Formato Usado Invalido");
+                    }
+
                 }
 
                 switch (i.target.innerHTML) {
+
                     case "+":
                         // display.innerHTML = currentOperand + previousOperand;
+                        operatorMathematical()
+                        updateDisplay();
                         break;
                     case "-":
+                        updateDisplay();
 
                         break;
-                    case "x":
+                    case "*":
+                        updateDisplay();
 
                         break;
                     case "÷":
+                        updateDisplay();
 
                         break;
-
                     case "C":
                         display.innerHTML = "0";
                         arr = [];
@@ -52,11 +85,14 @@ buttons.forEach(e => {
                         verificar(".");
                         break;
                     case "=":
+                        sinalInvalido("+");
+                        sinalInvalido("-");
+                        sinalInvalido("*");
+                        sinalInvalido("÷");
                         resultado.innerText = "";
-                        // resultado.innerText = //conta
-                        arr.forEach(num => {
-                            resultado.innerText = num;
-                        });
+                        // arr.forEach(num => {
+                        //     resultado.innerText = num;
+                        // });
                         break;
 
                     default:
--- scripts.js
+++ scripts.js
@@ -68,3 +68,79 @@ buttons.forEach(e => {
         console.log(arr);
     });
 });
+
+
+/*
+
+const buttons = document.querySelectorAll("button");
+let arr = [];
+let resultado = "";
+let display = document.querySelector(".resultado");
+const currentOperand = "";
+const previousOperand = "";
+
+
+buttons.forEach(e => {
+    e.addEventListener("click", i => {
+        const updateDisplay = () => {
+            resultado = i.target.innerHTML;
+            arr.push(resultado);
+            display.innerHTML = arr.join('');
+        }
+        switch (i.target.className) {
+            case "num":
+                updateDisplay();
+                break;
+            case "sinal":
+                const verificar = simbol => {
+                    if (arr.includes(simbol)) {
+                        console.log("sinal existente");
+                    } else {
+                        console.log("nao existe sinal");
+                        if (arr.length == 0) {
+                            arr.push(0);
+                        }
+                        resultado = i.target.innerHTML;
+                        arr.push(resultado);
+                        display.innerHTML = arr.join('');
+                    }
+                }
+
+                switch (i.target.innerHTML) {
+                    case "+":
+                        // display.innerHTML = currentOperand + previousOperand;
+                        break;
+                    case "-":
+
+                        break;
+                    case "x":
+
+                        break;
+                    case "÷":
+
+                        break;
+
+                    case "C":
+                        display.innerHTML = "0";
+                        arr = [];
+                        break;
+                    case ".":
+                        verificar(".");
+                        break;
+                    case "=":
+                        resultado.innerText = "";
+                        // resultado.innerText = //conta
+                        arr.forEach(num => {
+                            resultado.innerText = num;
+                        });
+                        break;
+
+                    default:
+                        break;
+                }
+        }
+        console.log(arr);
+    });
+});
+
+*/
